66ab4be9bdef26a415afcbcb3b7ed6d600c7eeb4
dodoproptit99 <dodo.proptit.99@gmail.com>
2019-07-13 06:49:54 +0700

    demo done

--- backend/ManhCuong/demo.py
+++ backend/ManhCuong/demo.py
@@ -1,43 +1,62 @@
 import numpy as np
 import os
 import queue
-from google_api import Text_to_speech
-from google_api import Speech_to_text 
+from thanhdatnv2712.google_api import googleAPI
 
 PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+user = googleAPI()
 
 class demo(object):
 
     def answer_0():
-        Text_to_speech("Mình có thể lưu trữ kiến thức của bạn, giúp bạn ôn tập kiến thức, giải đáp những câu hỏi của bạn dựa trên kho tri thức của nhân loại. Bạn muốn làm gì?")
+        user.setContent("Mình có thể lưu trữ kiến thức của bạn, giúp bạn ôn tập kiến thức, giải đáp những câu hỏi của bạn dựa trên kho tri thức của nhân loại. Bạn muốn làm gì?")
+        repath = user.text2speech()
+        return repath
 
     def answer_1():
-        Text_to_speech("Bạn muốn lưu vào thư mục nào?")
-
+        user.setContent("Bạn muốn lưu vào thư mục nào?")
+        repath = user.text2speech()
+        return repath
+       
     def answer_2():
-        Text_to_speech("Bạn có thể lựa chọn 3 cách để lưu kiến thức.")
+        user.setContent("Bạn có thể lựa chọn 3 cách để lưu kiến thức.")
+        repath = user.text2speech()
+        return repath
 
     def answer_2_1():
-        Text_to_speech("Mời bạn đặt câu hỏi.")
+        user.setContent("Mời bạn đặt câu hỏi.")
+        repath = user.text2speech()
+        return repath         
     
     def answer_2_2():
-        Text_to_speech("Mời bạn đặt câu trả lời.")
-    
+        user.setContent("Mời bạn đặt câu trả lời.")
+        repath = user.text2speech()
+        return repath 
+
     def answer_2_3():
-        Text_to_speech("Cám ơn bạn đã cung cấp thêm tri thức cho mình. Bạn có muốn đưa kiến thức này lên kho tri thức không?")
+        user.setContent("Cám ơn bạn đã cung cấp thêm tri thức cho mình. Bạn có muốn đưa kiến thức này lên kho tri thức không?")
+        repath = user.text2speech()
+        return repath 
 
     def answer_3():
-        Text_to_speech("Mình đã đưa lên thành công, vì câu hỏi của bạn có nhiều câu trả lời, bạn có thể upvote những câu trả lời chính xác nhất.")
+        user.setContent("Mình đã đưa lên thành công, vì câu hỏi của bạn có nhiều câu trả lời, bạn có thể upvote những câu trả lời chính xác nhất.")
+        repath = user.text2speech()
+        return repath
 
     def answer_4():
-        Text_to_speech("Bạn có cần mình giúp gì nữa không?")
+        user.setContent("Bạn có cần mình giúp gì nữa không?")
+        repath = user.text2speech()
+        return repath 
 
     def answer_5():
-        Text_to_speech("Theo câu trả lời của Andy, Rối loạn trầm cảm là loại rối loạn khí sắc thường gặp trong tâm thần học. Bệnh do hoạt động của bộ não bị rối loạn gây nên tạo thành những biến đổi thất thường trong suy nghĩ hành vi và tác phong.")
+        user.setContent("Theo câu trả lời của Andy, Rối loạn trầm cảm là loại rối loạn khí sắc thường gặp trong tâm thần học. Bệnh do hoạt động của bộ não bị rối loạn gây nên tạo thành những biến đổi thất thường trong suy nghĩ hành vi và tác phong.")
+        repath = user.text2speech()
+        return repath 
 
     def answer_wrong():
-        Text_to_speech("Mời bạn nói lại một lần nữa.")
+        user.setContent("Mời bạn nói lại một lần nữa.")
+        repath = user.text2speech()
+        return repath 
 
     def record(time):
         open_record(time)
@@ -47,7 +66,8 @@ class demo(object):
     def start(self):
         queue.Queue(maxsize=200)
         while start==true:
-            Text_to_speech("Xin chào, mình là eve. Mình có thể giúp gì cho bạn?")
+            user.setContent("Xin chào, mình là eve. Mình có thể giúp gì cho bạn?")
+            user.text2speech()
             input = record(time) # get stream audio
             answer_0() # giới thiệu tính năng
             input = record(time) # get stream audio
@@ -66,8 +86,11 @@ class demo(object):
             input_1 = record(time) 
             save_answer(input_1) #lưu câu hỏi vào db
             Queue2.put(input_1)
-            Text_to_speech(Queue2.get()) #đọc lại câu hỏi và câu trả lời    
-            Text_to_speech(Queue2.get())
+            #đọc lại câu hỏi và câu trả lời
+            user.setContent(Queue2.get())  
+            user.text2speech()
+            user.setContent(Queue2.get())
+            user.text2speech()
             answer_2_3()
             input = record(time) # yes
             answer_3() # đưa lên kho thành công
