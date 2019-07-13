33c450aac488655739fa3c2b0091e4d7dfa43b03
HanhNguyenDuc <nguyenduchanh99tnvn@gmail.com>
2019-07-13 08:42:12 +0700

    fixed orm

--- backend/hanhnd/views.py
+++ backend/hanhnd/views.py
@@ -10,9 +10,9 @@ def create_question(request, *args, **kwargs):
         owner_ = User.object.get(username = owner)
         
         if owner != None:
-            if Content.objects.filter(content = content).values("id") == None:
+            if Content.objects.get(content = content).id == None:
                 Content.objects.create(mean = content)
-                Questions.objects.create(content = Content.objects.filter(content = content).values("id")
+                Questions.objects.create(content = Content.objects.get(content = content).id
                 , user = owner_)
 
 
@@ -22,22 +22,22 @@ def create_answer(request, *args, **kwargs):
         owner = request.POST.get("owner")
         owner_ = User.objects.get(username = owner)
         if (owner_ != None):
-            if Content.object.filter(content = content).values("id") == None:
+            if Content.object.get(content = content).id == None:
                 Content.objects.create(mean = content)
-                id_content = Content.object.filter(content = content).values("id")
+                id_content = Content.object.get(content = content).id
                 Answers.objects.create(content = id, is_contrib = True, owner = owner_)
 
 def verified_answer(request, *args, **kwargs):
     if request.method == 'POST':
         content = request.POST.get("content")
-        id_content = Content.objects.filter(mean = content).values("id")
+        id_content = Content.objects.get(mean = content).id
         Answers.objects.filter(content = id).update(is_verified = True)
 
 def submit_answer(request, *args, **kwargs):
     if request.method == 'POST':
         question, answer = request.POST.get("question", "answer")
-        ques = Questions.objects.get(content = Content.objects.get(mean = question).values("id"))
-        ques.list_answers.add(Answers.objects.get(content = Content.objects.get(mean = answer).values("id")))
+        ques = Questions.objects.get(content = Content.objects.get(mean = question).id)
+        ques.list_answers.add(Answers.objects.get(content = Content.objects.get(mean = answer).id))
 
 
 
