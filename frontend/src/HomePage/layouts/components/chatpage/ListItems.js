e480ba5a0f0e453998fbd3455d21a4337784c87b
ntheanh201 <ntheanh201@gmail.com>
2019-07-14 02:33:07 +0700

    update ChatPage

--- frontend/src/HomePage/layouts/components/chatpage/ListItems.js
+++ frontend/src/HomePage/layouts/components/chatpage/ListItems.js
@@ -1,17 +1,22 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import {withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
+import Fab from '@material-ui/core/Fab';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
-import SocketConnect from './SocketConnect';
+import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreIcon from '@material-ui/icons/MoreVert';
 import styled from 'styled-components'
 
 const styles = theme => ({
@@ -54,68 +59,28 @@ const styles = theme => ({
 });
 
 class ListItems extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      messages: [{
-          user: 'BOT',
-          content: 'Xin chào, bạn cần tôi giúp đỡ gì không?'
-        }, {
-          user: 'me',
-          content: 'Xin chào'
-        }, {
-          user: 'BOT',
-          content: 'Xin chào, bạn cần tôi giúp đỡ gì không?'
-        }, {
-          user: 'me',
-          content: 'Xin chào'
-        }, 
-        {
-          user: 'BOT',
-          content: 'Xin chào, bạn cần tôi giúp đỡ gì không?'
-        }, {
-          user: 'me',
-          content: 'Xin chào'
-        }, 
-      ],
-    }
-  }
 
-  handleChange = (event) => {
-    this.setState({
-      messages: [...messages, ]
-    })
-  }
-
-  handleDeleteClick = (id) => {
-    const { messages } = this.state;
-    let arrMessages = messages;
-    arrMessages.splice(id, 1)
-    this.setState({ messages: arrMessages })
-  }
-
-  isBot = (user) => {
-    return (user === 'BOT') ? 'bot' : "me";
+  isBot = (name) => {
+    return (name === 'bot') ? 'bot' : "user";
   }
 
   render() {
     const { classes } = this.props;
-    const { messages } = this.state;
+    const { messages } = this.props;
     return (
       <Fragment>
         <Typography className={classes.text} variant="h5" gutterBottom>
           Messages
         </Typography>
         <CssBaseline />
-        <Paper square className={classes.paper}>
+        <StyledPaper square className={classes.paper}>
           <List className={classes.list}>
-            {messages.map(({ user, content }, index) => 
-              <Fragment key={index}><ListItem button><Avatar alt="Profile Picture" src={ '../../../../src/Assets/images/' + this.isBot(user)  +".png"} /> 
-               <StyledListItemText primary={user} secondary={content} /></ListItem></Fragment>
+            {messages.map(({ name, text }, index) =>
+              <Fragment key={index}><ListItem button><Avatar alt="Profile Picture" src={'../../../../src/Assets/images/' + this.isBot(name) + ".png"} />
+                <StyledListItemText primary={this.isBot(name) === 'user' ? 'Me' : 'Bot'} secondary={text} /></ListItem></Fragment>
             )}
           </List>
-        </Paper>
-        <SocketConnect />
+        </StyledPaper>
       </Fragment>
     );
   }
@@ -128,4 +93,9 @@ ListItems.propTypes = {
 export default withStyles(styles)(ListItems);
 const StyledListItemText = styled(ListItemText)`
   margin-left: 1%;
+`;
+
+const StyledPaper = styled(Paper)`
+  overflow: auto;
+  height: 620px;
 `;
--- frontend/src/HomePage/layouts/components/chatpage/SocketConnect.js
+++ frontend/src/HomePage/layouts/components/chatpage/SocketConnect.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import { ReactMic } from 'react-mic';
 import Sound from 'react-sound';
+import styled from 'styled-components'
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
 
 const client = new WebSocket("ws://127.0.0.1:8123/message/")
 
-export default class SocketConnect extends Component {
+const styles = theme => ({
+  fabButton: {
+    // position: 'absolute',
+    zIndex: 1,
+    top: -45,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    margin: '0 auto',
+  },
+  card: {
+    minWidth: 275,
+  },
+})
+
+class SocketConnect extends Component {
   constructor(props) {
 
     super(props);
@@ -52,9 +73,12 @@ export default class SocketConnect extends Component {
     }
     client.onmessage = (e) => {
       let responseData = JSON.parse(e.data);
+      const { createMessage } = this.props
+      createMessage(responseData);
       console.log(responseData);
+      console.log(responseData.user);
       let finish_status = false;
-      if(responseData.status == "finish"){
+      if (responseData.status == "finish") {
         finish_status = true;
       }
       this.setState({
@@ -69,30 +93,51 @@ export default class SocketConnect extends Component {
     this.setState({
       play_audio: Sound.status.STOPPED
     })
-    if(this.state.finish_conversation == false){
+    if (this.state.finish_conversation == false) {
       this.startRecording();
     }
   }
 
   render = () => {
+    const { classes } = this.props;
     return (
-      <div>
-        <ReactMic
-          mimeType="audio/webm;codecs=opus"
-          record={this.state.record}
-          className="sound-wave"
-          onStop={this.onStop}
-          onData={this.onData}
-          strokeColor="#000000"
-          backgroundColor="#FF4081" />
-        <button onClick={this.startRecording} type="button">Start</button>
-        <button onClick={this.stopRecording} type="button">Stop</button>
-        <Sound
-          url={this.state.audio_url}
-          playStatus={this.state.play_audio}
-          onFinishedPlaying={this.handleFinishAudio}
-        />
-      </div>
+      <Wrapper>
+        <Card className={classes.card}>
+          <ReactMic
+            mimeType="audio/webm;codecs=opus"
+            record={this.state.record}
+            className="sound-wave"
+            onStop={this.onStop}
+            onData={this.onData}
+            strokeColor="#000000"
+            backgroundColor="#FFFFFF" />
+          {/* <button onClick={this.startRecording} type="button">Start</button> */}
+          {/* <button onClick={this.stopRecording} type="button">Stop</button> */}
+
+          <Fab color="secondary" aria-label="Add" className={classes.fabButton}>
+            <AddIcon onClick={this.startRecording} />
+          </Fab>
+          <Sound
+            url={this.state.audio_url}
+            playStatus={this.state.play_audio}
+            onFinishedPlaying={this.handleFinishAudio}
+          />
+        </Card>
+      </Wrapper>
     );
   }
 }
+
+const Wrapper = styled.div`
+  bottom: -20px;
+  position: absolute;
+  left: 55%;
+  transform: translate(-50%, -50%);
+`;
+
+
+SocketConnect.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SocketConnect);
