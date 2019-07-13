fae5ba241b3ce8f100b6d1763fcf5eb446fc136b
dodoproptit99 <dodo.proptit.99@gmail.com>
2019-07-14 00:42:45 +0700

    fix demo

--- backend/ManhCuong/demo.py
+++ backend/ManhCuong/demo.py
@@ -9,6 +9,35 @@ user = googleAPI()
 
 class demo(object):
 
+    def __init__(self):
+        self.input = queue.Queue(maxsize=100)
+        self.output = queue.Queue(maxsize=100)
+
+    def process(input):
+        status = True
+        s = ""
+        if input == "bạn có thể làm những gì" :
+            s = "Mình có thể lưu trữ kiến thức của bạn, giúp bạn ôn tập kiến thức, giải đáp những câu hỏi của bạn dựa trên kho tri thức của nhân loại. Bạn muốn làm gì?"
+        if input == "mình muốn lưu lại kiến thức":
+            s = "Bạn muốn lưu vào thư mục nào?"
+        if input == "thư mục vật lí":
+            s = "Bạn có thể lựa chọn 3 cách để lưu kiến thức"
+        if input == "một câu hỏi ví dụ":
+            s = "Mời bạn đặt câu trả lời."
+        if input == "một câu trả lời ví dụ":
+            s = "Cám ơn bạn đã cung cấp thêm tri thức cho mình. Bạn có muốn đưa kiến thức này lên kho tri thức không?"
+        if input == "có":
+            s = "Mình đã đưa lên thành công, vì câu hỏi của bạn có nhiều câu trả lời, bạn có thể upvote những câu trả lời chính xác nhất."
+        if input == "trầm cảm là gì":
+            s = "Rối loạn trầm cảm là loại rối loạn khí sắc thường gặp trong tâm thần học. Bệnh do hoạt động của bộ não bị rối loạn gây nên tạo thành những biến đổi thất thường trong suy nghĩ hành vi và tác phong."
+        if input == "tôi muốn ôn tập lại kiến thức trong lĩnh vực lịch sử":
+            s = "Cách mạng tháng 10 Nga diễn ra vào thời gian nào, ai là người đứng đầu"
+        if input == "cách mạng tháng mười nổ ra vào ngày hai tư tháng mười năm một chín mười bảy, do lenin và đảng bon sê víc lãnh đạo":
+            s = "Chính xác, bạn có muốn ôn tập tiếp không?"
+        if input == "không":
+            status = False
+        return s, status
+    
     def answer_0():
         user.setContent("Mình có thể lưu trữ kiến thức của bạn, giúp bạn ôn tập kiến thức, giải đáp những câu hỏi của bạn dựa trên kho tri thức của nhân loại. Bạn muốn làm gì?")
         repath = user.text2speech()
@@ -35,7 +64,7 @@ class demo(object):
         return repath 
 
     def answer_2_3():
-        user.setContent("Cám ơn bạn đã cung cấp thêm tri thức cho mình. Bạn có muốn đưa kiến thức này lên kho tri thức không?")
+        user.strầm cảm là gìetContent("Cám ơn bạn đã cung cấp thêm tri thức cho mình. Bạn có muốn đưa kiến thức này lên kho tri thức không?")
         repath = user.text2speech()
         return repath 
 
@@ -96,48 +125,5 @@ class demo(object):
         open_record(time)
         input = Speech_to_text(path)
         return input
-
-    def start(self):
-        queue.Queue(maxsize=200)
-        while start==true:
-            user.setContent("Xin chào, mình là eve. Mình có thể giúp gì cho bạn?")
-            user.text2speech()
-            input = record(time) # get stream audio
-            answer_0() # giới thiệu tính năng
-            input = record(time) # get stream audio
-            answer_1() # hỏi tên thư mục muốn lưu
-            input_1 = record(time) #ghi lại tên thư mục
-            open_file(temp) #mở thư mục đó 
-            answer_2() #giới thiệu các cách lưu
-            display_options() # hiển thị bảng chọn
-            if select_status==true: close_display() # đóng bảng
-            queue.Queue2(maxsize=10)
-            answer_2_1() #đặt câu hỏi
-            input_1 = record(time) 
-            save_question(input_1) #lưu câu hỏi vào db
-            Queue2.put(input_1) 
-            answer_2_2() #đặt câu trả lời
-            input_1 = record(time) 
-            save_answer(input_1) #lưu câu hỏi vào db
-            Queue2.put(input_1)
-            #đọc lại câu hỏi và câu trả lời
-            user.setContent(Queue2.get())  
-            user.text2speech()
-            user.setContent(Queue2.get())
-            user.text2speech()
-            answer_2_3()
-            input = record(time) # yes
-            answer_3() # đưa lên kho thành công
-            display_table() #upvote câu trả lời
-            answer_4() #mình có thể giúp gì cho bạn
-            input = record(time) # trầm cảm là gì
-            answer_5() #trả lời
-            input = record(time) #tôi muốn ôn tập
-            answer_6() # lịch sử Nga
-            input = record(time) # câu trả lời
-            answer_7() #chính xác..
-            input = record(time) # câu trả lời
-            answer_4() #mình có thể giúp gì cho bạn
-            input = record(time) # không
 
 
